cbd5d7a081f6f71ec90a6a3ba20108b9831c5711
Thomas Wimmer <thomas.wimmer@netcetera.com>
2020-08-12 12:42:33 +0200

    Remove @ConfigurationPropertiesScan since not properties are used anymore

--- src/main/kotlin/com/thowimmer/axon/Application.kt
+++ src/main/kotlin/com/thowimmer/axon/Application.kt
@@ -3,13 +3,11 @@ package com.thowimmer.axon
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
-@ConfigurationPropertiesScan
 class Application
 
 fun main(args: Array<String>) {
